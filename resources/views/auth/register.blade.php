e56813ea28d4fafad106f7e02d9e3a2f425d230a
Julia De Jesus <julsvdj01@gmail.com>
2021-08-30 14:50:17 +0800

    updated two css classes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="--background-img">
+    <div class="--dark-overlay"></div>
+</div>
+
+<div class="container --overlap mb-5">
+    <div class="row">
+        <div class="col-4 offset-4">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
