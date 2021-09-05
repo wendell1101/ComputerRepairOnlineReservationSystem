349f20ca092fef4d73602a717c351c232349fdef
Julia De Jesus <julsvdj01@gmail.com>
2021-09-05 11:41:09 +0800

    modified servicefees page

--- resources/views/user/service-fees.blade.php
+++ resources/views/user/service-fees.blade.php
@@ -5,8 +5,7 @@
     <div class="--dark-overlay"></div>
 </div>
 
-<div class="container" data-spy="scroll" data-target="#nav-scroll" data-offset="0">
-    <h1 class="text-center --roboto-condensed --bold mb-3" >Service Fees</h1>
+<div class="container">
     <div class="row" >
         <div class="col-md-3">
             <nav id="side-scroll"></nav>
@@ -17,6 +16,7 @@
             </div>
         </div>
         <div class="col-md-9">
+            <h1 class="text-center --roboto-condensed --bold mb-3" >Service Fees</h1>
             <div >
                 <section id="repair-section" class="mb-5">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium modi, facere consequatur necessitatibus, molestias illum pariatur suscipit quasi magnam, voluptatibus nobis dolore beatae quae blanditiis quod. Natus ab voluptate pariatur blanditiis autem libero sed nobis hic dolore laborum officia sequi optio reiciendis debitis magni, porro accusamus! Voluptates, sint? Debitis, vitae. Repellat nesciunt ducimus nam est repudiandae ex repellendus! Aspernatur facere sunt rem voluptatibus odit tenetur, consequuntur quas cum modi enim aut debitis maxime esse recusandae ab ducimus autem ipsum. Quaerat ratione earum minus nesciunt vel reprehenderit neque sapiente iure sequi beatae, mollitia autem fugit deleniti molestiae aspernatur quibusdam eligendi et vero tempore obcaecati dolor quisquam! Delectus sunt, placeat, nisi commodi expedita debitis deleniti labore quia eligendi fugit aperiam provident eos facilis ut harum! Cumque nulla aliquam quisquam est illum, qui autem optio sapiente, impedit expedita deleniti id quae porro! Nostrum, quae perspiciatis nulla praesentium magnam nesciunt obcaecati possimus fugiat, placeat voluptatum corporis iure aut iste perferendis laborum quaerat, ipsam ratione omnis? Hic inventore eveniet, sequi sit porro accusantium minus, modi laborum, cumque molestias iste! Neque corporis at, dolorum quas commodi perferendis molestias perspiciatis beatae placeat voluptatibus qui ratione libero odio minus nobis repudiandae optio, mollitia ea ipsa aspernatur. Dolorum, omnis.</p>
