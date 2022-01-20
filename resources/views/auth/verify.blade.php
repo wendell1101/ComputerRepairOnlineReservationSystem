1e0b77394523e06148d05dcd74cdde97f26a514d
Julia De Jesus <julsvdj01@gmail.com>
2022-01-20 15:41:41 +0800

    updated uis of auth pages

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,25 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="--background-img">
+    <div class="--dark-overlay"></div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+            @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('A fresh verification link has been sent to your email address.') }}
+                </div>
+            @endif
+            <div class="row no-gutters shadow">
+                <div class="col-md-3 col-sm-12 --bg-gray-800 --text-green d-flex justify-content-center align-items-center p-3">
+                    <h1 class="mb-0">
+                        <i class="fas fa-envelope"></i>
+                    </h1>
+                </div>
+                <div class="col-md-9 col-sm-12 --text-gray-800 --bg-gray-50 p-4">
+                    <h3 class="--poppins --bold">{{ __('Verify Your Email Address') }}</h3>
+                    <p class="--roboto-condensed --body-18">
+                        Before proceeding, please check your email for verification link. If you did not receive the email, click the button below to request another.
+                        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                            @csrf
+                            <button type="submit" class="btn --btn-outline-green">Click here to request another</button>.
+                        </form>
+                    </p>
                 </div>
             </div>
         </div>
